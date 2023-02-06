e30ea42932bce541e7334464f704d403e86708f6
chela-droid <[200~chelangatcaroline70@gmail.com~>
2023-02-06 23:37:26 +0300

    commit tree 6a0025102e712a53ab32f238848ae41f84a1d964

--- methods.rb
+++ methods.rb
@@ -1,1 +1,24 @@
 # Your code here!
+def greet_programmer 
+    puts"Hello, programmer!"
+end
+
+
+def greet(name="Naureen")
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name="programmer")
+    puts "Hello, #{name}!"
+end
+
+def add num1,num2
+    return 2+5
+end
+
+def halve(number)
+if number !=6
+return nil
+end
+return number/2
+end
